cb071f76e9bfc666270e6ece77aeafe035b69a1f
btnguyenPersonal <btnguyenPersonal@github.com>
2023-07-06 19:55:59 -0500

    seems to be working

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const ytdl = require('ytdl-core');
+const ytpl = require('ytpl');
 const ffmpeg = require('fluent-ffmpeg');
 const ffmpegPath = require('ffmpeg-static');
+const fs = require('fs');
+const path = require('path');
+const archiver = require('archiver');
 
 const app = express();
 const port = 4000;
@@ -9,31 +13,82 @@ const port = 4000;
 ffmpeg.setFfmpegPath(ffmpegPath);
 
 app.get('/download', async (req, res) => {
-  try {
-    const url = req.query.url;
-    if (!ytdl.validateURL(url)) {
-      return res.sendStatus(400);
+    try {
+        const url = req.query.url;
+        const videoDir = path.join(__dirname, '/videos');
+        const isPlaylist = ytpl.validateID(url);
+
+        if (!isPlaylist && !ytdl.validateURL(url)) {
+            return res.sendStatus(400);
+        }
+
+        if (!fs.existsSync(videoDir)) {
+            fs.mkdirSync(videoDir);
+        }
+
+        let videos = [];
+
+        let downloadTitle = 'title';
+        if (isPlaylist) {
+            const playlist = await ytpl(url);
+            downloadTitle = playlist.title;
+            videos = playlist.items;
+        } else {
+            const info = await ytdl.getInfo(url);
+            downloadTitle = info.player_response.videoDetails.title;
+            videos = [info.videoDetails];
+        }
+
+        const downloadVideo = (video) => new Promise((resolve, reject) => {
+            let title = video.title.replace(/[^a-zA-Z ]/g, "");
+            const writeStream = fs.createWriteStream(path.join(videoDir, `${title}.mp3`));
+            const videoReadStream = ytdl(video.url || url, { quality: 'highestaudio' });
+
+            ffmpeg(videoReadStream)
+                .audioBitrate(128)
+                .format('mp3')
+                .pipe(writeStream)
+                .on('finish', resolve)
+                .on('error', reject);
+        });
+
+        for (const video of videos) {
+            await downloadVideo(video);
+        }
+        const archive = archiver('zip');
+        const output = fs.createWriteStream(path.join(__dirname, `${downloadTitle}.zip`));
+
+        archive.pipe(output);
+        archive.directory(videoDir, false);
+        archive.finalize();
+
+        output.on('close', () => {
+            res.download(path.join(__dirname, `${downloadTitle}.zip`), (err) => {
+                if (err) {
+                    console.error(err);
+                    res.sendStatus(500);
+                } else {
+                    fs.readdir(videoDir, (err, files) => {
+                        if (err) throw err;
+                        for (const file of files) {
+                            fs.unlink(path.join(videoDir, file), err => {
+                                if (err) throw err;
+                            });
+                        }
+                        fs.unlink(path.join(__dirname, `${downloadTitle}.zip`), err => {
+                            if (err) throw err;
+                        });
+                    });
+                }
+            });
+        });
+
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
     }
-    let title = 'audio';
-
-    await ytdl.getInfo(url).then(info => {
-      title = info.videoDetails.title.replace(/[^a-zA-Z ]/g, "");
-    });
-
-    res.header('Content-Disposition', `attachment; filename="${title}.mp3"`);
-    const videoReadStream = ytdl(url, { quality: 'highestaudio' });
-
-    ffmpeg(videoReadStream)
-      .audioBitrate(128)
-      .format('mp3')
-      .pipe(res);
-
-  } catch (err) {
-    console.error(err);
-    res.sendStatus(500);
-  }
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
--- frontend/src/VideoDownload.tsx
+++ frontend/src/VideoDownload.tsx
@@ -1,19 +1,20 @@
 import React, { useState } from 'react';
 
 const VideoDownload = () => {
-  const [url, setUrl] = useState('');
+    const [url, setUrl] = useState('');
 
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-    window.open(`http://localhost:4000/download?url=${url}`);
-  };
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        const encodedUrl = encodeURIComponent(url);
+        window.open(`http://localhost:4000/download?url=${encodedUrl}`);
+    };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={url} onChange={e => setUrl(e.target.value)} placeholder="Enter YouTube video URL" required />
-      <button type="submit">Download</button>
-    </form>
-  );
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={url} onChange={e => setUrl(e.target.value)} placeholder="Enter YouTube video URL" required />
+            <button type="submit">Download</button>
+        </form>
+    );
 };
 
 export default VideoDownload;
